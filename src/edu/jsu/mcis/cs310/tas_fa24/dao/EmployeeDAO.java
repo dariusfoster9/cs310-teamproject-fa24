4b459d14629de57c3362fec9a6ce965726df7e6d
William sellers <wsellers@stu.jsu.edu>
2024-10-07 12:02:21 -0500

    unpdated Empolyee

--- src/edu/jsu/mcis/cs310/tas_fa24/dao/EmployeeDAO.java
+++ src/edu/jsu/mcis/cs310/tas_fa24/dao/EmployeeDAO.java
@@ -14,19 +14,23 @@ import edu.jsu.mcis.cs310.tas_fa24.*;
 
 public class EmployeeDAO {
      
-    private Connection conn;
+    private DAOFactory daoFactory;
 
-    public EmployeeDAO(Connection conn) {
-        this.conn = conn;
+    public EmployeeDAO(DAOFactory daoFactory) {
+        this.daoFactory = daoFactory;
     }
 
     public Employee find(int id) {
 
         Employee employee = null;
         
+        
         try {
+             
+            Connection conn = daoFactory.getConnection();
             String query = "SELECT * FROM employee WHERE id = ?";
             PreparedStatement ps = conn.prepareStatement(query);
+         
             ps.setInt(1, id);
             ResultSet rs = ps.executeQuery();
             
@@ -36,9 +40,9 @@ public class EmployeeDAO {
                 String middlename = rs.getString("middlename");
                 String lastname = rs.getString("lastname");
                 LocalDateTime active = rs.getTimestamp("active").toLocalDateTime();
-                Badge badge = new BadgeDAO(conn).find(rs.getString("badgeid"));
-                Department department = new DepartmentDAO(conn).find(rs.getInt("departmentid"));
-                Shift shift = new ShiftDAO(conn).find(rs.getInt("shiftid"));
+                Badge badge = new BadgeDAO(daoFactory).find(rs.getString("badgeid"));
+                Department department = new DepartmentDAO(daoFactory).find(rs.getInt("departmentid"));
+                Shift shift = new ShiftDAO(daoFactory).find(rs.getInt("shiftid"));
                 EmployeeType employeeType = EmployeeType.values()[rs.getInt("employeetype")];
                 
                 employee = new Employee(id, firstname, middlename, lastname, active, badge, department, shift, employeeType);
@@ -55,6 +59,7 @@ public class EmployeeDAO {
         Employee employee = null;
 
         try {
+            Connection conn = daoFactory.getConnection();
             String query = "SELECT id FROM employee WHERE badgeid = ?";
             PreparedStatement ps = conn.prepareStatement(query);
             ps.setString(1, badge.getId());
