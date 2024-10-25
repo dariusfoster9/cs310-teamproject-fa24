61b9b665f990ea024e0659bbda90f0ff326eb35b
Corey Dunn <coreyd8899@gmail.com>
2024-10-24 20:43:33 -0500

    create finish

--- test/edu/jsu/mcis/cs310/tas_fa24/PunchCreateTest.java
+++ test/edu/jsu/mcis/cs310/tas_fa24/PunchCreateTest.java
@@ -20,34 +20,34 @@ public class PunchCreateTest {
 
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-        // Get DAOs
+        
         PunchDAO punchDAO = daoFactory.getPunchDAO();
         BadgeDAO badgeDAO = daoFactory.getBadgeDAO();
 
-        /* Create New Punch Object */
-        Badge badge = badgeDAO.find("021890C0");  // Retrieve Badge object
-        Punch p1 = new Punch(103, badge.getId(), 1);  // Create Punch with terminal ID, badge ID, and event type ID
+        
+        Badge badge = badgeDAO.find("021890C0");  
+        Punch p1 = new Punch(103, badge.getId(), 1);  
 
-        /* Create Timestamp Objects */
+        
         LocalDateTime ots, rts;
 
-        /* Get Punch Properties */
-        String badgeid = p1.getBadgeid();  // Use getBadgeid() to retrieve the badge ID
-        ots = p1.gettimestamp();   // Get the original timestamp
-        int terminalid = p1.getterminalid();  // Get the terminal ID
-        int eventTypeId = p1.geteventtypeId();  // Get the event type ID
+        
+        String badgeid = p1.getBadgeid();  
+        ots = p1.getTimestamp();   
+        int terminalid = p1.getTerminalid();  
+        int eventTypeId = p1.getEventTypeId();  
 
-        /* Insert Punch Into Database */
+        
         int punchid = punchDAO.create(p1);
 
-        /* Retrieve New Punch */
+        
         Punch p2 = punchDAO.find(punchid);
 
-        /* Compare Punches */
-        assertEquals(badgeid, p2.getBadgeid());   // Compare badge IDs
-        rts = p2.getOriginaltimestamp();          // Get the original timestamp from the retrieved punch
-        assertEquals(terminalid, p2.getTerminalid());    // Compare terminal IDs
-        assertEquals(eventTypeId, p2.getEventTypeId());  // Compare event type IDs
-        assertEquals(ots.format(dtf), rts.format(dtf));  // Compare timestamps (formatted)
+        
+        assertEquals(badgeid, p2.getBadgeid());   
+        rts = p2.getTimestamp();          
+        assertEquals(terminalid, p2.getTerminalid());    
+         
+        assertEquals(ots.format(dtf), rts.format(dtf));  
     }
 }
